d144b979c7dc3d5ce2f7cb75ef35ff00ac802fa0
andrejMarkovic97 <markovic.andrej97@gmail.com>
2022-03-21 18:01:09 +0100

    Add header and footer html file and link them to other html pages

---
+++ html/footer.php
@@ -0,0 +1,94 @@
+<section class=" footer" id="footer">
+
+    <footer class="text-center text-lg-start bg-light text-muted">
+
+        <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+
+
+        </section>
+
+        <section class="">
+            <div class="container text-center text-md-start mt-5">
+
+                <div class="row mt-3">
+
+                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            <i class="fas fa-gem me-3"></i>Nice With Spice
+                        </h6>
+                        <p>
+                            Geared toward making cooking a more enjoyable experience for you!
+                        </p>
+                    </div>
+
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Social media
+                        </h6>
+                        <p>
+                            <a href="#!" class="text-reset"><i class="bi bi-instagram"> Instagram</i></a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset"><i class="bi bi-facebook"> Facebook</i></a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset"><i class="bi bi-twitter"> Twitter</i></a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset"><i class="bi bi-linkedin"> LinkedIn</i></a>
+                        </p>
+
+                    </div>
+
+
+
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Useful links
+                        </h6>
+                        <p>
+                            <a href="#!" class="text-reset">Pricing</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Settings</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Orders</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Help</a>
+                        </p>
+                    </div>
+
+
+
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Contact
+                        </h6>
+                        <p>Belgrade, Vojvode Stepe 355</p>
+                        <p>nicewithspice@gmail.com</p>
+                        <p>+ 381 11 123 456</p>
+                        <p>+381 61 123 6789</p>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+
+
+
+        <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+            © 2022 Copyright:
+            <p>Andrej Marković</p>
+        </div>
+
+    </footer>
+
+</section>
---
+++ html/header.php
@@ -0,0 +1,123 @@
+<nav class="navbar navbar-expand-lg">
+    <div class="container">
+        <a class="navbar-brand" href="#">Nice With Spice</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="bi bi-chevron-double-down"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarScroll">
+            <ul class="navbar-nav m-auto my-2 my-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" href='landing.php'>Home</a>
+                </li>
+                <li class=" nav-item">
+                    <a class="nav-link" href="store.php">Store</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#footer">
+                        Contact us
+                    </a>
+
+                </li>
+
+            </ul>
+            <span class="cart">
+                <a class="nav-link active" href="cart.php">
+                    <i class="bi bi-cart3"></i>
+                </a>
+            </span>
+
+            <div class="dropdown">
+                <span class="cart">
+                    <a class="nav-link active dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false" style="color:black;">
+                        <i class="bi bi-person-circle bi-4x"></i>
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <li><a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Update account</a></li>
+                        <li><a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#deleteAccount">Delete account</a></li>
+                        <li><a class="dropdown-item" href="#">Log out</a></li>
+                    </ul>
+                </span>
+
+            </div>
+
+
+
+            <form class="d-flex">
+
+                <input class="px-2 search" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn0" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="card-body p-5 text-center">
+                        <h3 class="mb-5">Update account info</h3>
+                        <form method="POST" action="">
+                            <div class="form-outline mb-4">
+                                <input type="text" name="name" id="typeNameX-2" class="form-control form-control-lg" required />
+                                <label class="form-label" for="typeNameX-2">First And Last Name</label>
+                            </div>
+
+
+                            <div class="form-outline mb-4">
+                                <input type="email" name="email" id="typeEmailX-2" class="form-control form-control-lg" required />
+                                <label class="form-label" for="typeEmailX-2">Email</label>
+                            </div>
+
+                            <div class="form-outline mb-4">
+                                <input type="password" name="password" id="typePasswordX-2" class="form-control form-control-lg" required />
+                                <label class="form-label" for="typePasswordX-2">Password</label>
+                            </div>
+
+                            <div class="form-outline mb-4">
+                                <input type="password" id="typePasswordX-2" class="form-control form-control-lg" required />
+                                <label class="form-label" for="typePasswordX-2">Retype Password</label>
+                            </div>
+
+
+                            <button class="btn btn-primary btn-lg btn-block" name="submit" type="submit">Confirm</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="deleteAccount" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="card-body p-5 text-center">
+                        <h3 class="mb-5">Delete account</h3>
+                        <h5 class="mb-5">Are you sure?</h5>
+                        <form method="POST" action="">
+
+                            <div class="form-outline mb-4">
+                                <input type="password" name="password" id="typePasswordX-2" class="form-control form-control-lg" required />
+                                <label class="form-label" for="typePasswordX-2">Password</label>
+                            </div>
+
+                            <div class="form-outline mb-4">
+                                <input type="password" id="typePasswordX-2" class="form-control form-control-lg" required />
+                                <label class="form-label" for="typePasswordX-2">Retype Password</label>
+                            </div>
+
+
+                            <button class="btn btn-danger btn-lg btn-block" name="submit" type="submit">Confirm</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
